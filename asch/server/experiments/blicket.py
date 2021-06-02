44a459553d2c7195922ee7c0352006e33bb8fd4c
Adrian Liu <adrianliu99@berkeley.edu>
2021-06-01 17:04:47 -0700

    slight modification to blicket.py

--- asch/server/experiments/blicket.py
+++ asch/server/experiments/blicket.py
@@ -2,6 +2,32 @@ from .base import BaseGame
 
 import random
 
+task1 = {
+    "type" : "inference",
+    "name" : "conjunctive_train",
+    "maxObjectsOnDetector": 3,
+    "shuffle" : False,
+    "rule": "conjunctive",
+    "detector_pattern": 0,
+    "blicket_arrangement": [1, 0, 1],
+    "blicket_colors": [9, 3, 4],
+    "blicket_shapes": [0, 1, 2]
+}
+
+task2 = {
+    "type" : "inference",
+    "name" : "conjunctive_test",
+    "maxObjectsOnDetector": 3,
+    "shuffle" : True,
+    "rule": "conjunctive",
+    "detector_pattern": 2,
+    "blicket_arrangement": [1, 0, 1],
+    "blicket_colors": [6, 7, 8],
+    "blicket_shapes": [6, 7, 8]
+}
+
+tasks = [task1, task2]
+
 
 class Blicket(BaseGame):
 
@@ -19,28 +45,4 @@ class Blicket(BaseGame):
 
     @classmethod
     def new_experiment_sequence(cls, condition: str):
-
-        # Get a random pair of objects which form the blickets. The game handles the rest.
-        objects = [0, 1, 2]
-        random.shuffle(objects)
-        blickets = set(objects[:2])
-
-        return [{
-            "name" : "conjunctive_train",
-            "maxObjectsOnDetector": 3,
-            "shuffle" : False,
-            "rule": "conjunctive",
-            "detector_pattern": 0,
-            "blicket_arrangement": [1, 0, 1],
-            "blicket_colors": [0, 1, 2],
-            "blicket_shapes": [0, 1, 2]
-        },{
-            "name" : "conjunctive_test",
-            "maxObjectsOnDetector": 3,
-            "shuffle" : True,
-            "rule": "conjunctive",
-            "detector_pattern": 2,
-            "blicket_arrangement": [1, 0, 1],
-            "blicket_colors": [6, 7, 8],
-            "blicket_shapes": [6, 7, 8]
-        }]
+        return tasks
