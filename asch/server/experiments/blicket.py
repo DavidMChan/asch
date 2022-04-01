9b8cd6acd90d162d4b6494aca73bb07b2f683f65
Adrian Liu <adrianliu99@berkeley.edu>
2022-03-31 20:04:35 -0700

    fixed aspect ratio of game, and blicket.py fix

--- asch/server/experiments/blicket.py
+++ asch/server/experiments/blicket.py
@@ -58,7 +58,7 @@ conditions = {
         "blicket_arrangement": [1, 0, 1],
         "blicket_colors": [0, 1, 2],
         "blicket_shapes": [0, 1, 2]
-    }
+    },
     'B6': {
         "type": "inference",
         "name": "B6_conjunctive_train_dots",
@@ -146,7 +146,7 @@ class Blicket(BaseGame):
 
     @classmethod
     def conditions(cls):
-        return ['B1', 'B2', 'B3', 'B4', 'B5']
+        return ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B6', 'C1', 'C2', 'C3', 'C4']
 
     @classmethod
     def new_experiment_sequence(cls, condition: str):
